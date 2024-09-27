693a9861794843ff72b346c27fca18b3ee791ab6
Dariusz D <darekduda21@gmail.com>
2024-09-26 23:51:57 -0400

    feat: add fzf to the configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -155,4 +155,19 @@
       crust = "#181926";
     };
   };
+
+  # fzf integration
+  programs.fzf.enable = true;
+  programs.fzf.colors = {
+    "spinner" = "#f4dbd6";
+    hl = "#ed8796";
+    fg = "#cad3f5";
+    header = "#ed8796";
+    info = "#c6a0f6";
+    pointer = "#f4dbd6";
+    marker = "#f4dbd6";
+    "fg+" = "#cad3f5";
+    prompt = "#c6a0f6";
+    "hl+" = "#ed8798";
+  };
 }
