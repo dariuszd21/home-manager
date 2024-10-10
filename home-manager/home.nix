f894ce9e4ee357df88273bd8a14e58b1d9e08c3d
Dariusz D <darekduda21@gmail.com>
2024-10-09 23:04:31 -0400

    feat: add support for OpenGL applications

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,5 +1,4 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, ... }: {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "dariuszd";
@@ -16,7 +15,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -33,12 +32,14 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-    pkgs.zig
-    pkgs.fira-code-nerdfont
+    zig
+    fira-code-nerdfont
 
     # Wayland native snapshot editing tool
-    pkgs.grim
-    pkgs.swappy
+    grim
+    swappy
+
+    nixgl.nixGLMesa
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
