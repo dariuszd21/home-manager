864162ac17eef2056c77c8e4606720f79b2abf98
Dariusz D <darekduda21@gmail.com>
2024-11-17 16:44:17 -0500

    chore: add keybindings to zsh

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -96,10 +96,48 @@
       fi
       export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin:$HOME/.local/bin
       export LS_COLORS="$(vivid generate catppuccin-macchiato)"
+    '';
 
+    initExtra = ''
+      typeset -g -A key
+
+      key[Home]="''${terminfo[khome]}"
+      key[End]="''${terminfo[kend]}"
+      key[Insert]="''${terminfo[kich1]}"
+      key[Backspace]="''${terminfo[kbs]}"
+      key[Delete]="''${terminfo[kdch1]}"
+      key[Up]="''${terminfo[kcuu1]}"
+      key[Down]="''${terminfo[kcud1]}"
+      key[Left]="''${terminfo[kcub1]}"
+      key[Right]="''${terminfo[kcuf1]}"
+      key[PageUp]="''${terminfo[kpp]}"
+      key[PageDown]="''${terminfo[knp]}"
+      key[Shift-Tab]="''${terminfo[kcbt]}"
+
+      # setup key accordingly
+      [[ -n "''${key[Home]}"      ]] && bindkey -- "''${key[Home]}"       beginning-of-line
+      [[ -n "''${key[End]}"       ]] && bindkey -- "''${key[End]}"        end-of-line
+      [[ -n "''${key[Insert]}"    ]] && bindkey -- "''${key[Insert]}"     overwrite-mode
+      [[ -n "''${key[Backspace]}" ]] && bindkey -- "''${key[Backspace]}"  backward-delete-char
+      [[ -n "''${key[Delete]}"    ]] && bindkey -- "''${key[Delete]}"     delete-char
+      [[ -n "''${key[Up]}"        ]] && bindkey -- "''${key[Up]}"         up-line-or-history
+      [[ -n "''${key[Down]}"      ]] && bindkey -- "''${key[Down]}"       down-line-or-history
+      [[ -n "''${key[Left]}"      ]] && bindkey -- "''${key[Left]}"       backward-char
+      [[ -n "''${key[Right]}"     ]] && bindkey -- "''${key[Right]}"      forward-char
+      [[ -n "''${key[PageUp]}"    ]] && bindkey -- "''${key[PageUp]}"     beginning-of-buffer-or-history
+      [[ -n "''${key[PageDown]}"  ]] && bindkey -- "''${key[PageDown]}"   end-of-buffer-or-history
+      [[ -n "''${key[Shift-Tab]}" ]] && bindkey -- "''${key[Shift-Tab]}"  reverse-menu-complete
+
+
+      # bind word jumping
+      key[Control-Left]="''${terminfo[kLFT5]}"
+      key[Control-Right]="''${terminfo[kRIT5]}"
+
+      [[ -n "''${key[Control-Left]}"  ]] && bindkey -- "''${key[Control-Left]}"  backward-word
+      [[ -n "''${key[Control-Right]}" ]] && bindkey -- "''${key[Control-Right]}" forward-word
     '';
     # initExtraBeforeCompInit = ''
-    #   fpath=($fpath ~/.zsh/completions)
+    #   fpath+=(~/.zsh/completions)
     # '';
   };
 }
