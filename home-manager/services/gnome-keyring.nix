7453342fa83d2ea2c5ea1bd0104f5edebee07c01
Dariusz D <darekduda21@gmail.com>
2024-11-24 18:55:54 -0500

    fix: enable gnome-keyring to store nix credentials
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- home-manager/services/gnome-keyring.nix
+++ home-manager/services/gnome-keyring.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  home.packages = with pkgs; [ gcr ];
+
   # Add gnome-keyring daemon
-  services.gnome-keyring = { enable = true; };
+  services.gnome-keyring = {
+    enable = true;
+    components = [ "pkcs11" "secrets" ];
+  };
 }
