ce7ab34c9a014c6f30f48e447fd05c177badf482
Dariusz D <darekduda21@gmail.com>
2025-10-31 12:58:50 +0100

    chore: update git config
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -4,17 +4,20 @@
   # Enable git
   programs.git = {
     enable = true;
-    aliases = {
-      last = "log -1 --stat";
-      cleanup =
-        "!git branch --merged | grep  -v '\\*\\|main\\|develop' | xargs -n 1 git branch -d";
-      cs = "commit --signoff --gpg-sign";
+    settings = {
+      aliases = {
+        last = "log -1 --stat";
+        cleanup = "!git branch --merged | grep  -v '\\*\\|main\\|develop' | xargs -n 1 git branch -d";
+        cs = "commit --signoff --gpg-sign";
+      };
+      init = {
+        defaultBranch = "develop";
+      };
+      # TODO: check fancy diffs in the future
+      # delta.enable = true;
+      # diff-highlight.enable = true;
+      # diff-so-fancy.enable = true;
+      # difftastic.enable  = true;
     };
-    # TODO: check fancy diffs in the future
-    # delta.enable = true;
-    # diff-highlight.enable = true;
-    # diff-so-fancy.enable = true;
-    # difftastic.enable  = true;
-    extraConfig = { init = { defaultBranch = "develop"; }; };
   };
 }
