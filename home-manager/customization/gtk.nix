2964323460ba7ff6dae802753a7214a58f1d45f8
Dariusz D <darekduda21@gmail.com>
2025-11-24 15:51:46 +0100

    feat: set icons for GTK4 apps
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- home-manager/customization/gtk.nix
+++ home-manager/customization/gtk.nix
@@ -1,21 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  # config.gtk.catppuccin = {
-  #   accent = "flamingo";
-  #   flavor = "macchiato";
-  # };
+  config = {
+    gtk = {
+      enable = true;
+      colorScheme = "light";
 
-  config.gtk = {
-    enable = true;
-    colorScheme = "light";
+      theme = {
+        name = "catppuccin-latte-sky-standard";
+        package = pkgs.catppuccin-gtk.override {
+          accents = [ "sky" ];
+          size = "standard";
+          variant = "latte";
+        };
+      };
+      iconTheme = {
+        name = "Papirus";
+        package = pkgs.papirus-icon-theme;
+      };
+    };
 
-    theme = {
-      name = "catppuccin-latte-sky-standard";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "sky" ];
-        size = "standard";
-        variant = "latte";
+    dconf = {
+      enable = true;
+      settings = {
+        "org/gnome/desktop/interface" = {
+          icon-theme = "Papirus";
+          gtk-theme = "catppuccin-latte-sky-standard";
+          font-name = "Fira Code Retina 10";
+          color-scheme = "prefer-light";
+        };
       };
     };
   };
