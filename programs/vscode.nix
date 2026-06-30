ab604a29080d83dee81f9ea891db9742dd2826ee
Dariusz D <9116056+dariuszd21@users.noreply.github.com>
2026-06-30 09:59:15 +0200

    chore(vscode): remove sway gnome-libsecret workaround

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -6,14 +6,6 @@
 }:
 
 {
-  home.file.".vscode/argv.json".text = ''
-    {
-      // Fixes the "an OS keyring couldn't be identified for
-      // storing the encryption..." error
-      "password-store": "gnome-libsecret"
-    }
-  '';
-
   # vscode integration
   programs.vscode = {
     enable = true;
